92d54143256cad87ad6bdcb8bb0c10064da512d8
Minh Dang <nhatminhdang04@gmail.com>
2020-07-07 12:28:09 -0400

    update script.js

--- script.js
+++ script.js
@@ -3,19 +3,14 @@ const topRight = document.querySelector('.top-right');
 const bottomLeft = document.querySelector('.bottom-left');
 const bottomRight = document.querySelector('.bottom-right');
 
-const getRandomPanel = () => {
-  const panels = [
-    topLeft,
-    topRight,
-    bottomLeft,
-    bottomRight
-  ];
+const randomPanel = () => {
+  const panels = [topLeft, topRight, bottomLeft, bottomRight];
   return panels[parseInt(Math.random() * panels.length)];
 };
 
-const sequence = [getRandomPanel()];
-let sequenceToGuess = [...sequence]; //clone
-console.log(sequenceToGuess);
+const sequence = [randomPanel()];
+let guess = [...sequence]; //clone
+//console.log(guess);
 
 const flash = panel => {
   return new Promise(resolve => {
@@ -29,31 +24,31 @@ const flash = panel => {
   });
 };
 
-let canClick = false;
+let click = false;
 
-const panelClicked = panelClicked => {
-  if (!canClick) return; //return from the callback preventing player to click when still flashing
-  const expectedPanel = sequenceToGuess.shift();
-  console.log(expectedPanel);
-  if (expectedPanel === panelClicked) {
-    if (sequenceToGuess.length === 0) {
-      // start new round
-      sequence.push(getRandomPanel());
-      sequenceToGuess = [...sequence];
+const panelClicking = clickedPanel => {
+  if (!click) return; //return from the callback preventing player to click when still flashing
+  const expectedPanel = guess.shift();
+  //console.log(expectedPanel);
+  if (expectedPanel === clickedPanel) {
+    if (guess.length === 0) {
+      // New round
+      sequence.push(randomPanel());
+      guess = [...sequence];
       startFlashing();
     }
   } else {
-    // end game
-    alert('game over');
+    // End
+    alert('Wrong guessing! You lost.');
   }
 };
 
 const startFlashing = async () => {
-  canClick = false;
+  click = false;
   for (const panel of sequence) {
     await flash(panel); //prevent flashing on the same time
   }
-  canClick = true;
+  click = true;
 };
 
 startFlashing();
